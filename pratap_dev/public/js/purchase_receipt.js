9a0efd2562fc4ad2f205facbcabb59426cf32a11
Abhishek Kumar <abhishek.suvaidyam@gmail.com>
2026-06-08 09:07:09 +0000

    add changes

--- pratap_dev/public/js/purchase_receipt.js
+++ pratap_dev/public/js/purchase_receipt.js
@@ -1,6 +1,6 @@
-// frappe.ui.form.on("Purchase Receipt", {
-//     refresh(frm) {
-//         frm.page.remove_inner_button(__("Quality Inspection(s)"));
-//         console.log("refreshing remove_inner_button");
-//     }
-// });
+frappe.ui.form.on("Purchase Receipt", {
+    refresh(frm) {
+        // remove the core button to create quality inspection as per Pratap Quality Inspection requirements
+        frm.page.remove_inner_button(__("Quality Inspection(s)"));
+    }
+});
