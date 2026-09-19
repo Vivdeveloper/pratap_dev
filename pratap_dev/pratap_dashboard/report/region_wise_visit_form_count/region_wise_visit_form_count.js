7352834f15c2f5dbf7373c83238f788f661b817b
Abhishek Kumar <abhishek.suvaidyam@gmail.com>
2026-09-19 06:59:02 +0000

    add changes

---
+++ pratap_dev/pratap_dashboard/report/region_wise_visit_form_count/region_wise_visit_form_count.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, saurabh@exacuer.com and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Region Wise Visit Form count"] = {
+	"filters": [
+
+	]
+};
